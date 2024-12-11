9e9cd0ba7bd945bbb56602ff24f08de7d4d1c9f0
Naveen-Raj-M <naveenrajmanoharan@gmail.com>
2024-12-11 10:12:47 -0600

    modify venv

--- build_venv.sh
+++ build_venv.sh
@@ -6,5 +6,11 @@ python3 -m venv find_your_state
 # Activate the virtual environment
 source find_your_state/bin/activate
 
+# Check Python version
+which python
+
+# Upgrade pip
+python -m pip install --upgrade pip
+
 # Install packages from requirements.txt
 pip install -r requirements.txt
